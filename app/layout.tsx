29c340f7934c9cdad3819d6e5ee9770f11e0e234
Duarti <duartnishefci@gmail.com>
2023-12-20 15:17:49 +0100

    fixed import issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SWRProvider } from "./(app)/providers/swr-provider";
-import { ToasterProvider } from "./(app)/providers/ToasterProvider";
 import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
